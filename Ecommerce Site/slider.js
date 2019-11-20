ebc968a6ca6a6159edb9e47fb70609def14dd133
mfontanilla98 <39034680+mfontanilla98@users.noreply.github.com>
2019-11-20 11:36:03 -0800

    Ecommerce Website
    Demonstrating usage of flexboxes for css layout as well as Javascript for minor functionality.

---
+++ Ecommerce Site/slider.js
@@ -0,0 +1,8 @@
+function myFunction(imgs){
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
